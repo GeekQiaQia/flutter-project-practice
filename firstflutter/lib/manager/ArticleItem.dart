86f473a02a52dfcbf36a49da0f72352a6d963757
apple <apple@appledeMacBook-Pro.local>
2020-11-01 12:23:18 +0800

    manger-layout-article

--- firstflutter/lib/manager/ArticleItem.dart
+++ firstflutter/lib/manager/ArticleItem.dart
@@ -9,98 +9,78 @@ class ArticleItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
       padding: EdgeInsets.all(5),
       child: Card(
-        elevation: 5,
-        child: Padding(
-          padding: EdgeInsets.all(5),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              new Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Container(
-                        width: 40,
-                        height: 40,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            image: DecorationImage(
-                                image: NetworkImage(model.logo),
-                                fit: BoxFit.cover)),
+          child: new Column(
+        children: [
+          new Row(
+            children: [
+              Padding(
+                  padding: EdgeInsets.only(
+                      left: 15.0, right: 15.0, top: 10.0, bottom: 0),
+                  child: Image.network(model.logo, width: 50.0, height: 50.0)),
+              Padding(
+                  padding: EdgeInsets.only(
+                      top: 5.0, right: 5.0, bottom: 5.0, left: 0),
+                  child: Text(
+                      model.location.substring(
+                          0,
+                          model.location.length > 6
+                              ? 6
+                              : model.location.length),
+                      style: TextStyle(fontSize: 13, color: Colors.grey))),
+              Padding(
+                  padding: EdgeInsets.only(
+                      top: 5.0, right: 10.0, bottom: 5.0, left: 5.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "|" + model.type,
+                        style: TextStyle(fontSize: 13, color: Colors.grey),
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.center,
                       ),
                       Text(
-                        model.location,
-                        style: TextStyle(fontSize: 12, color: Colors.grey),
+                        "|" + model.size,
+                        style: TextStyle(fontSize: 13, color: Colors.grey),
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.center,
                       ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            model.type,
-                            style: TextStyle(fontSize: 12, color: Colors.grey),
-                          ),
-                          Text(
-                            model.size,
-                            style: TextStyle(fontSize: 12, color: Colors.grey),
-                          ),
-                          Text(
-                            model.employee,
-                            style: TextStyle(fontSize: 12, color: Colors.grey),
-                          ),
-                        ],
+                      Text(
+                        "|" + model.employee,
+                        style: TextStyle(fontSize: 13, color: Colors.grey),
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.center,
                       )
                     ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text(
-                            '热招：',
-                            style: TextStyle(fontSize: 12, color: Colors.grey),
-                          ),
-                          Text(
-                            model.hot,
-                            style: TextStyle(fontSize: 12, color: Colors.grey),
-                          ),
-                          SizedBox(width: 10),
-                          Text(
-                            '等：',
-                            style: TextStyle(fontSize: 12, color: Colors.grey),
-                          ),
-                          Text(
-                            model.count,
-                            style: TextStyle(fontSize: 12, color: Colors.grey),
-                          ),
-                          Text(
-                            '个职位',
-                            style: TextStyle(fontSize: 12, color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Icon(Icons.arrow_forward_rounded,
-                              size: 20, color: Colors.grey),
-                        ],
-                      )
-                    ],
-                  )
-                ],
-              ),
+                  ))
             ],
           ),
-        ),
-      ),
+          Divider(),
+          new Row(
+            children: [
+              Padding(
+                  padding: EdgeInsets.only(
+                      top: 5.0, right: 5.0, bottom: 15.0, left: 15.0),
+                  child: new Text(
+                      "热招:" + model.hot + "等" + model.count + "多个职位",
+                      style:
+                          new TextStyle(fontSize: 13.0, color: Colors.grey))),
+              Expanded(
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  new Padding(
+                    padding: EdgeInsets.only(bottom: 8),
+                    child: Icon(Icons.chevron_right, color: Colors.grey),
+                  ),
+                ],
+              ))
+            ],
+          )
+        ],
+      )),
     );
   }
 }
